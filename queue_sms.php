e8e4bdda4526cd978f4a5483894e3d446851748b
Henry <henrybentil88@gmail.com>
2022-07-08 09:07:31 +0000

    fixes:worked on add variables to text

--- queue_sms.php
+++ queue_sms.php
@@ -25,7 +25,7 @@ $channel->queue_declare(
 # TODO: Replace json_encode(array('message'=> 'Hello World!")) with the details of the
 # smses to send
 //open uploaded csv file with read only mode
-$csvFile = fopen('./files/ANMA_2022BILL_SMSBLAST_FINAL_COMMERCIAL.csv', 'r');
+$csvFile = fopen('./files/ANMA_2022BILL_SMSBLAST_FINAL_RESIDENTIAL.csv', 'r');
 
 // skip first line
 // if your csv file have no heading, just comment the next line
@@ -33,9 +33,11 @@ fgetcsv($csvFile);
 
 $assembly = "Akuapem North Municipal Assembly";
 $assembly_abbrv = "ANMA";
-$bill_type = "Commercial Property Rate";
+$bill_type = "Residential Property Rate";
 $year = "2022";
-$phone_no = "0541214224";
+$phone_no1 = "0541214224";
+$phone_no2 = "0551500055";
+
 
 
 
@@ -45,7 +47,7 @@ while(($line = fgetcsv($csvFile)) !== FALSE){
     }else{
         $msg = new AMQPMessage(
             // json_encode(array('message' => "Dear Customer \nYour ".$line[1]." Bill for ". $line[2]." for ".$line[3]." is GHs".number_format((float)$line[4], 0, '.', '')."; Arrears is GHs".number_format((float)$line[5], 0, '.', '')."; Total Bill is GHs".number_format((float)$line[6], 0, '.', '').". Plz pay at any nearest DEKSOL office, Pay point or collector. Insist on your receipt for any payment made. MoMo#: 0547168973. Call after every MoMo transfer to get your receipt.\nThank U", "number" => $line[0])),
-            json_encode(array('message' => "Dear Customer \nGreetings from ".$assembly.".\nYour ".$year." ".$bill_type." Bill for ". $line[2]." is Ghs".number_format((float)$line[4], 0, '.', '').", Arrears is Ghs".number_format((float)$line[5], 0, '.', '')."\nTotal Bill is GHs".number_format((float)$line[6], 0, '.', '').".\nPlz pay at any nearest ".$assembly_abbrv." office, Paypoint or collector. Insist on your receipt for any payment. Plz call ".$phone_no." if you have any issue or if your bill has been paid already for the necessary corrections to be made on your account.\nThank U", "number" => $line[0])),
+            json_encode(array('message' => "Dear Customer \nGreetings from ".$assembly.".\nYour ".$year." ".$bill_type." Bill for ". $line[2]." is Ghs".number_format((float)$line[4], 0, '.', '').", Arrears is Ghs".number_format((float)$line[5], 0, '.', '')."\nTotal Bill is GHs".number_format((float)$line[6], 0, '.', '').".\nPlz pay at any nearest ".$assembly_abbrv." office, Paypoint or collector. Insist on your receipt for any payment. Plz call ".$phone_no1." or ".$phone_no2." if you have any issue or if your bill has been paid already for the necessary corrections to be made on your account.\nThank U", "number" => $line[0])),
             array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
         );
     
